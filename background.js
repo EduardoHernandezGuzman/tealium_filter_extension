3aacf75dc78c9e2fdee8b4a10cab97350c0e5af8
Edo <139759297+EduardoHernandezGuzman@users.noreply.github.com>
2024-10-14 09:30:23 +0200

    Update background.js

--- background.js
+++ background.js
@@ -2,9 +2,9 @@ let utagEvents = [];
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.type === "utagEvent") {
-        utagEvents.push({utagEvents: message.data});
+        utagEvents.push(message.data);
         console.log("utagEvent capturado:", message.data);
     } else if (message.type === "getUtagEvents") {
-        sendResponse({ utagEvents: utagEvents });
+        sendResponse(utagEvents);
     }
 });
