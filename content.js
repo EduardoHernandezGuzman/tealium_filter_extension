2e0f3fab0a6529181fdc525ab649892fcde523de
Edo <139759297+EduardoHernandezGuzman@users.noreply.github.com>
2024-10-14 10:17:05 +0200

    Update content.js

--- content.js
+++ content.js
@@ -1,9 +1,30 @@
-const originalConsoleLog = console.log;
-console.log = function(...args) {
-    originalConsoleLog.apply(console, args);
-    const isUtagView = args.some(arg => typeof arg === 'string' && arg.includes('utag.view'));
-    const isUtagLink = args.some(arg => typeof arg === 'string' && arg.includes('utag.link'));
-    if (isUtagView || isUtagLink) {
-        chrome.runtime.sendMessage({type: "utagEvent", data: args});
+const scriptContent = `
+    // Guarda la función original
+    const originalUtagView = utag.view;
+
+    // Sobrescribe la función
+    utag.view = function(data) {
+        // Llama a la función original
+        originalUtagView.apply(this, arguments);
+
+        // Envía los datos del evento a la extensión
+        window.postMessage({ type: "FROM_PAGE", text: JSON.stringify(data) }, "*");
+    };
+`;
+
+// Inyecta el script en la página
+const scriptEl = document.createElement('script');
+scriptEl.textContent = scriptContent;
+(document.head||document.documentElement).appendChild(scriptEl);
+scriptEl.remove();
+
+// Escucha los mensajes del script inyectado
+window.addEventListener("message", function(event) {
+    // Solo aceptamos mensajes de nosotros mismos
+    if (event.source != window)
+        return;
+
+    if (event.data.type && (event.data.type == "FROM_PAGE")) {
+        chrome.runtime.sendMessage({type: "utagEvent", data: event.data.text});
     }
-};
+}, false);
