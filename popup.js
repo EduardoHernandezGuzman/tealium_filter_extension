0e6b2598656b8da52287eaaf24b7fa4b960cabeb
Edo <139759297+EduardoHernandezGuzman@users.noreply.github.com>
2024-10-14 09:30:39 +0200

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,12 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
     chrome.runtime.sendMessage({ type: "getUtagEvents" }, (response) => {
-        console.log('Response:', response);
         const eventsList = document.getElementById('eventsList');
-        if (response && response.utagEvents) {
-            console.log('Utag events:', response.utagEvents);
-            response.utagEvents.forEach(event => {
+        if (response) {
+            response.forEach(event => {
                 const li = document.createElement('li');
-                li.textContent = event.utagEvents;
+                li.textContent = event;
                 eventsList.appendChild(li);
             });
         }
