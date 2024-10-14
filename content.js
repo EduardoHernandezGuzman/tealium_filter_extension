78669381e28ac3fdc427f498262fa7dce9b01aeb
Edo <139759297+EduardoHernandezGuzman@users.noreply.github.com>
2024-10-14 09:07:02 +0200

    Update content.js

--- content.js
+++ content.js
@@ -1,14 +1,27 @@
-(function() {
+const scriptContent = `
     const originalConsoleLog = console.log;
-
     console.log = function(...args) {
         originalConsoleLog.apply(console, args);
-
         const isUtagView = args.some(arg => typeof arg === 'string' && arg.includes('utag.view'));
         const isUtagLink = args.some(arg => typeof arg === 'string' && arg.includes('utag.link'));
-
         if (isUtagView || isUtagLink) {
-            chrome.runtime.sendMessage({ type: "utagEvent", data: args });
+            window.postMessage({ type: "FROM_PAGE", text: args }, "*");
         }
     };
-})();
+`;
+
+const scriptEl = document.createElement('script');
+scriptEl.textContent = scriptContent;
+(document.head||document.documentElement).appendChild(scriptEl);
+scriptEl.remove();
+
+window.addEventListener("message", function(event) {
+    // We only accept messages from ourselves
+    if (event.source != window)
+        return;
+
+    if (event.data.type && (event.data.type == "FROM_PAGE")) {
+        console.log("Content script received: " + event.data.text);
+        chrome.runtime.sendMessage({type: "utagEvent", data: event.data.text});
+    }
+}, false);
