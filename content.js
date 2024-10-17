0f9f4516267f84fff90c9384fe4b69da42d341c2
Edo <139759297+EduardoHernandezGuzman@users.noreply.github.com>
2024-10-17 09:58:52 +0200

    Update content.js

--- content.js
+++ content.js
@@ -1,24 +1,19 @@
 const scriptContent = `
-    // Guarda la función original
     const originalUtagView = utag.view;
 
-    // Sobrescribe la función
     utag.view = function(data) {
         // Llama a la función original
         originalUtagView.apply(this, arguments);
 
-        // Envía los datos del evento a la extensión
         window.postMessage({ type: "FROM_PAGE", text: JSON.stringify(data) }, "*");
     };
 `;
 
-// Inyecta el script en la página
 const scriptEl = document.createElement('script');
 scriptEl.textContent = scriptContent;
 (document.head||document.documentElement).appendChild(scriptEl);
 scriptEl.remove();
 
-// Escucha los mensajes del script inyectado
 window.addEventListener("message", function(event) {
     // Solo aceptamos mensajes de nosotros mismos
     if (event.source != window)
